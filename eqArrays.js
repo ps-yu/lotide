f75e4ce6a726d00dcd944bc61c01abd366085203
ps-yu <pankajsharma12540@gmail.com>
2022-10-29 19:48:51 -0400

    creating new function

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const eqArrays = (firstArray,secondArray) => {
+  if(firstArray.length !== secondArray.length){
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++){
+    if (firstArray[i] !== secondArray[i]){
+      return false;
+    }
+  }return true;
+}
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
+const assertEqual = function(actual, expected){
+  if (actual === expected){
+    return `\u2705\u2705\u2705 Assertion passed : ${actual} === ${expected}`;
+  }else {
+    return `\u274C\u274C\u274CAssertion Failed : ${actual} !=== ${expected}`;
+  }
+}
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
