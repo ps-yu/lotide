a102ffe78135050abc1e863eceaa0323f45318f7
ps-yu <pankajsharma12540@gmail.com>
2022-12-11 21:43:32 -0500

    Add all functions

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const tail   = require('./tail');
 const middle = require('./middle');
 const findKey = require('./findKey');
 const map = require('/map');
+const assertEqual = require('/assertEqual');
+const countLetters = require('/countLetters');
+const flatten = require('/flatten');
+const map = require('/map');
+
 
 module.exports = {
   head,
   tail,
   middle,
   findKey,
-  map
+  map,
+  assertEqual,
+  countLetters,
+  flatten
 };
