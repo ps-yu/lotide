71c9f3a2b5323f16142ec0c9d8dc9aef5b6cfa2c
ps-yu <pankajsharma12540@gmail.com>
2023-01-15 15:42:20 -0500

    Give the right description

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,10 @@
 // const assertEqual = require('../assertEqual')
 // const tail = require('../tail')
-
-const head = require('../head');
-
 // console.log(assertEqual(tail([12,3,4,56,"LHL"]),'LHL'))
  const assert = require('chai').assert;
  const tail = require('../tail')
 
- describe('#test', ()=> {
+ describe('#tail', ()=> {
   it('returns 4 for [12,3,4]', () => {
     assert.strictEqual(tail([12,3,4]),4)
   })
